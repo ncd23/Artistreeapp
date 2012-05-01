f6e015cc13b417fcfabf4551f18358aa80e876f0
Galina Rybatsky <rybag047@newschoool.edu>
2012-04-30 20:14:02 -0400

    Initial Commit

---
+++ Artistreeapp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Artistreeapp
+//
+//  Created by Galina Rybatsky on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Artistreeapp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Artistreeapp
+//
+//  Created by Galina Rybatsky on 4/30/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
